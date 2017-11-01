a1b14cbfc18b12cd107781f59da44552b90c8712
gipsh <gipshm@gmail.com>
2017-11-01 04:15:21 +0200

    Update omniauth.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,11 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, 'my google client id', 'my google client secret', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
+  provider :google_oauth2, 
+      'my google client id', 
+      'my google client secret', 
+      {  access_type: "offline",
+         prompt: "consent",
+         scope: 'email, profile, plus.me, http://gdata.youtube.com',
+         client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
 end
